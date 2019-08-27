84668ffbcf20831c0aa26bddf179e3e9e40cd112
Jiho Lee <warezio@msn.com>
2019-08-27 00:20:35 -0700

    Update js format

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import Layout from "../components/layout";
+import Image from "../components/image";
+
 const AboutPage = () => (
 	<main>
 		<h1>About the Author</h1>
 		<p>Welcome to my Gatsby site.</p>
-    <Link to="/">Go to Home</Link>
+		<Link to="/">Go back to the homepage</Link>
 	</main>
 );
 
